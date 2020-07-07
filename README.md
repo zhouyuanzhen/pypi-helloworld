ae41b88f8eee2d23c348f23b6f2e6014233a857a
Zhou Yuanzhen <szrednick+zhouyuanzhen@gmail.com>
2020-07-07 23:32:54 +0800

    Refactor to zyzhi

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 
 This is a demo pypi helloworld project to show a PyPI project end2end workflow.
 
+## QuickStart
+
+```python
+from zyzhi import zyzhi
+
+zyzhi.sayhi()
+
+```
+
 Enjoy!
--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with io.open('README.md', 'rt', encoding='utf8') as f:
     readme = f.read()
 
 setup(
-    name='zyzhelloworld',
+    name='zyzhi',
     license='Apache License 2.0',
-    version='0.1.4',
+    version='0.2.1',
     maintainer='Yuanzhen Zhou',
     maintainer_email='szrednick@gmail.com',
-    packages=find_packages(include=['helloworld', 'helloworld.*']),
+    packages=find_packages(include=['zyzhi', 'zyzhi.*']),
     zip_safe=False,
     include_package_data=True,
     url='https://github.com/zhouyuanzhen/pypi-helloworld',
